287ca4fcd0bdc187a1ea0a18d604becab530c08b
Pete Bounford <petebounford@gmail.com>
2017-03-03 10:42:57 +0100

    Create empty DropIt project with custom view controller and view with no implementation yet

---
+++ DropIt/DropItViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DropItViewController.swift
+//  DropIt
+//
+//  Created by Pete Bounford on 03/03/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import UIKit
+
+class DropItViewController: UIViewController {
+
+}
