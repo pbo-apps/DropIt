15894a306c19476ff7ba1c2869433ee7185fe8a9
Pete Bounford <petebounford@gmail.com>
2017-03-03 11:23:28 +0100

    Add gravity and collision behaviours to the drop it views to get rudimentary block dropping game working

--- DropIt/DropItView.swift
+++ DropIt/DropItView.swift
@@ -10,4 +10,46 @@ import UIKit
 
 class DropItView: UIView {
 
+    private let gravity = UIGravityBehavior()
+    
+    // Initialise the collider using a closure to allow us to configure it with a boundary
+    private let collider: UICollisionBehavior = {
+        let collider = UICollisionBehavior()
+        collider.translatesReferenceBoundsIntoBoundary = true
+        return collider
+    }()
+    
+    private lazy var animator: UIDynamicAnimator = UIDynamicAnimator(referenceView: self)
+    
+    var animating = false {
+        didSet {
+            if animating {
+                animator.addBehavior(gravity)
+                animator.addBehavior(collider)
+            } else {
+                animator.removeBehavior(gravity)
+                animator.removeBehavior(collider)
+            }
+        }
+    }
+    
+    private let dropsPerRow = 10
+    
+    private var dropSize: CGSize {
+        let size = bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+    
+    func addDrop() {
+        var frame = CGRect(origin: CGPoint.zero, size: dropSize)
+        frame.origin.x = CGFloat.random(max: dropsPerRow) * dropSize.width
+    
+        let drop = UIView(frame: frame)
+        drop.backgroundColor = UIColor.random
+        
+        addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+    } 
+    
 }
--- DropIt/DropItViewController.swift
+++ DropIt/DropItViewController.swift
@@ -10,4 +10,31 @@ import UIKit
 
 class DropItViewController: UIViewController {
 
+    @IBOutlet weak var gameView: DropItView! {
+        didSet {
+            gameView.addGestureRecognizer(
+                UITapGestureRecognizer(
+                    target: self,
+                    action: #selector(addDrop(_:))
+                )
+            )
+        }
+    }
+    
+    func addDrop(_ recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .ended {
+            gameView.addDrop()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        gameView.animating = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        gameView.animating = false
+    }
+    
 }
